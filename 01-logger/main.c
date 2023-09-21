a710bbc9c3b808746fed1c93c40757d2d27253cf
Saeid Aliei <saeidaliei@gmail.com>
2023-09-21 13:52:12 +0200

    add main file

---
+++ 01-logger/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "logging.h"
+
+int main()
+{
+    /*logging::Logging logger;*/
+    /*logger.setLevel(logging::ERROR);*/
+    /*logger.setFilepath("./log.txt");*/
+    /*logger.setName("root");*/
+
+    /*logger.debug("this is a test debug msg");*/
+    /*logger.info("this is a test info msg");*/
+    /*logger.warn("this is a test warn msg");*/
+    /*logger.error("this is a test error msg");*/
+    /*logger.critical("this is a test critical msg");*/
+
+    printf("this is a test from main.\n");
+    return 0;
+
+}
