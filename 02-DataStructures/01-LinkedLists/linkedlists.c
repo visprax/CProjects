9f0fdd90ad30d2f29369272859daf40bbfb0f6aa
Saeid Aliei <saeidaliei@gmail.com>
2023-09-22 23:53:01 +0200

    add push_back and free_list functions

--- 02-DataStructures/01-LinkedLists/linkedlists.c
+++ 02-DataStructures/01-LinkedLists/linkedlists.c
@@ -3,34 +3,70 @@
 
 #include "linkedlists.h"
 
-void add_single_node(SinglyLinkedList* slist, double data) {
-    SingleNode* new_snode = (SingleNode*)malloc(sizeof(SingleNode));
-    new_snode->data = data;
-    new_snode->next = NULL; // this is the latest node we have added
+static SingleNode* create_single_node(double data) {
+    SingleNode* new_node = (SingleNode*)malloc(sizeof(SingleNode));
+    new_node->data = data;
+    // by default when a node is created the next pointer is NULL
+    new_node->next = NULL; 
 
+    return new_node;
+}
+
+void push_back(SinglyLinkedList* list, double data) {
+    SingleNode* new_node = create_single_node(data);
+
+    // if the head of list is NULL, this list is empty
+    if(list->head == NULL) {
+        list->head = new_node;
+    }
+    // otherwise traverse to the end and add
+    else {
+        SingleNode* last_node = list->head;
+        while(last_node->next != NULL) {
+            last_node = last_node->next;
+        }
+        last_node->next = new_node;
+    }
+}
+
+void print_list(SinglyLinkedList* list) {
+    SingleNode* current_node = list->head;
+    while(current_node != NULL) {
+        printf("%.2lf-->", current_node->data);
+        current_node = current_node->next;
+    }
+    printf("NULL\n");
 
 }
 
-void add_double_node(DoublyLinkedList* dlist, double data) {
-    DoubleNode* new_dnode = (DoubleNode*)malloc(sizeof(DoubleNode));
-    new_dnode->data = data;
-    new_dnode->next = dlist->head;
-
-
+void free_list(SinglyLinkedList* list) {
+    while(list->head != NULL) {
+        SingleNode* tmp = list->head;
+        list->head = tmp->next;
+        free(tmp);
+    }
 }
 
 int main() {
-    SingleNode snode;
-    snode.data = 1.0;
-    snode.next = NULL;
+    /*SingleNode snode;*/
+    /*snode.data = 1.0;*/
+    /*snode.next = NULL;*/
 
-    DoubleNode dnode;
-    dnode.data = 2.0;
-    dnode.next = NULL;
-    dnode.prev = NULL;
+    /*DoubleNode dnode;*/
+    /*dnode.data = 2.0;*/
+    /*dnode.next = NULL;*/
+    /*dnode.prev = NULL;*/
 
+    /*printf("%lf\n", snode.data);*/
+    /*printf("%lf\n", dnode.data);*/
 
-    printf("%lf\n", snode.data);
-    printf("%lf\n", dnode.data);
+    SinglyLinkedList list;
+    list.head = NULL;
+    push_back(&list, 1.0);
+    push_back(&list, 2.0);
+    push_back(&list, 3.0);
+    print_list(&list);
+    free_list(&list);
+
     return 0;
 }
